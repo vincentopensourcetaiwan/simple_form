5f480101fc91a1cc6b3abf628e77d75a68755913
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-25 15:11:46 +0800

    setup simple form

--- lib/simple_form_web/live/home_live.ex
+++ lib/simple_form_web/live/home_live.ex
@@ -1,9 +1,22 @@
 defmodule SimpleFormWeb.HomeLive do
   use SimpleFormWeb, :live_view
+  alias SimpleForm.Contents
+  alias SimpleForm.Contents.Tag
 
   def render(assigns) do
     ~H"""
     <h1>Welcome to SimpleForm!</h1>
+    <.simple_form for={@form}>
+      <.input field={@form[:name]} type="text" label="Name" />
+      <.button>save</.button>
+    </.simple_form>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    change_set = Contents.change_tag(%Tag{})
+    form = to_form(change_set)
+    socket = assign(socket, form: form)
+    {:ok, socket}
   end
 end
